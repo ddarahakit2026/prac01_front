eeebbefd3aae08dcea58ea2ae965e05e966a40bc
ddarahakit2026 <ddarahakit2026@gmail.com>
2026-02-24 10:39:23 +0900

    [Feat] 게시글 작성 기능 구현
    axiosintercepter 설정 및 게시글 작성 구현
    게시글 작성은 title, contents만 구현

--- src/plugins/axiosinterceptor.js
+++ src/plugins/axiosinterceptor.js
@@ -3,8 +3,8 @@ import axios from 'axios'
 axios.defaults.withCredentials = true
 
 const api = axios.create({
-  // baseURL: 'http://10.10.10.20:8080',
-  baseURL: '/api',
+  baseURL: 'http://localhost:8080',
+  // baseURL: '/api',
   timeout: 5000,
 })
 
--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,13 @@ import { createRouter, createWebHistory } from 'vue-router'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+    {
+      name: 'boardReg',
+      path: '/board/reg',
+      component: () => import('@/views/board/Reg.vue'),
+    },
+  ],
 })
 
 export default router
